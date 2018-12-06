658a67b1c84a5766bb39d96ba5315b8cbb44e8d2
12ColcloughJ <12colcloughj@lytchett.org.uk>
2018-12-06 16:50:04 +0000

    Distance used to get pull of gravity.

--- main.lua
+++ main.lua
@@ -1,25 +1,24 @@
-
 require "misc"
 require "vec2"
 require "planet"
 
 function love.load()
-	love.window.setMode(400, 400)
-	
-	planets = {}
-	Planet(1, Vec2(100, 100), Vec2(0, 1), 10, 25)
-	Planet(2, Vec2(300, 300), Vec2(0, -1), 10, 25)
-	Planet(3, Vec2(350, 200), Vec2(0, 0), 10, 25)
+    love.window.setMode(400, 400)
+
+    planets = {}
+    Planet(1, Vec2(150, 150), Vec2(0, 0), 10, 25)
+    Planet(2, Vec2(250, 250), Vec2(0, 0), 10, 25)
+    --Planet(3, Vec2(350, 200), Vec2(0, 0), 10, 25)
 end
 
 function love.update()
-	for i=1, #planets do
-		planets[i]:update()
-	end
+    for i=1, #planets do
+        planets[i]:update()
+    end
 end
 
 function love.draw()
-	for i=1, #planets do
-		planets[i]:draw()
-	end
+    for i=1, #planets do
+        planets[i]:draw()
+    end
 end
--- planet.lua
+++ planet.lua
@@ -1,58 +1,59 @@
 
 function Planet(id, pos, vel, m, r)
-	local p  = {}
-	p.id     = id
-	p.pos    = pos or Vec2(0, 0)
-	p.vel    = vel or Vec2(0, 0)
-	p.mass   = m
-	p.radius = r
-	
-	function p:getPlanetForces()
-		local x_force, y_force = 0, 0
-		
-		for i=1, #planets do
-			if i ~= self.id then
-				local other = planets[i]
-				local angle = getAngle(self.pos.x, self.pos.y, other.pos.x, other.pos.y)
-				
-				local mag     = 0.05
-				x_force = x_force + math.cos(angle)*mag
-				y_force = y_force + math.sin(angle)*mag
-			end
-		end
-		
-		x_force = x_force / (#planets-1)
-		y_force = y_force / (#planets-1)
-		
-		return x_force, y_force
-	end
-	
-	function p:applyXForce(dx)
-		self.vel.x = self.vel.x + dx
-	end
-	
-	function p:applyYForce(dy)
-		self.vel.y = self.vel.y + dy
-	end
-	
-	function p:applyVel()
-		self.pos.x = self.pos.x + self.vel.x
-		self.pos.y = self.pos.y + self.vel.y
-	end
-	
-	function p:update()
-		local x_force, y_force = self:getPlanetForces()
-		
-		self:applyXForce(x_force)
-		self:applyYForce(y_force)
-		self:applyVel()
-	end
-	
-	function p:draw()
-		love.graphics.setColor({255, 255, 255})
-		love.graphics.circle("line", self.pos.x, self.pos.y, self.radius)
-	end
-	
-	table.insert(planets, p)
-	return p
+    local p  = {}
+    p.id     = id
+    p.pos    = pos or Vec2(0, 0)
+    p.vel    = vel or Vec2(0, 0)
+    p.mass   = m
+    p.radius = r
+
+    function p:getPlanetForces()
+        local x_force, y_force = 0, 0
+
+        for i=1, #planets do
+            if i ~= self.id then
+                local angle = getAngle(self.pos.x, self.pos.y, planets[i].pos.x, planets[i].pos.y)
+
+                local dist  = ((self.pos.x - planets[i].pos.x)^2) + ((self.pos.y - planets[i].pos.x)^2)^0.5     -- Pythag meme
+                print(dist, "Dist")
+
+                x_force = x_force + math.cos(angle)*(1/dist)
+                y_force = y_force + math.sin(angle)*(1/dist)
+            end
+        end
+
+        x_force = x_force / (#planets-1)
+        y_force = y_force / (#planets-1)
+
+        return x_force, y_force
+    end
+
+    function p:applyXForce(dx)
+        self.vel.x = self.vel.x + dx
+    end
+
+    function p:applyYForce(dy)
+        self.vel.y = self.vel.y + dy
+    end
+
+    function p:applyVel()
+        self.pos.x = self.pos.x + self.vel.x
+        self.pos.y = self.pos.y + self.vel.y
+    end
+
+    function p:update()
+        local x_force, y_force = self:getPlanetForces()
+
+        self:applyXForce(x_force)
+        self:applyYForce(y_force)
+        self:applyVel()
+    end
+
+    function p:draw()
+        love.graphics.setColor({255, 255, 255})
+        love.graphics.circle("line", self.pos.x, self.pos.y, self.radius)
+    end
+
+    table.insert(planets, p)
+    return p
 end
