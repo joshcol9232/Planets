743dc0796a178ea02bb8b5ac21e4257060ee4e38
12ColcloughJ <12colcloughj@lytchett.org.uk>
2018-12-12 20:05:47 +0000

    Eh kind of works not really

--- main.lua
+++ main.lua
@@ -1,8 +1,9 @@
 require "planet"
 require "vec2"
-require "misc"
 
-G = 6.67*(10^-4)
+G = 6.67*(10^-3)
+
+lg = love.graphics
 
 function love.load()
   love.window.setMode(700, 700)
@@ -10,15 +11,16 @@ function love.load()
   love.physics.setMeter(1)
   world = love.physics.newWorld(0, 0, true)
   planets = {}
-  Planet(1, Vec2(100, 100), 10)
-  Planet(2, Vec2(300, 300), 100)
+  Planet(1, Vec2(300, 300), Vec2(0, 0), 50, 1000)
+  Planet(2, Vec2(300, 100), Vec2(200, 0), 20, 100)
+  --Planet(4, Vec2(400, 100), Vec2(0, 0), 1, 100)
 end
 
 function love.update(dt)
   world:update(dt)
-  --for i=1, #planets do
-    --planets[i]:update()
-  --end
+  for i=1, #planets do
+    planets[i]:update()
+  end
 end
 
 function love.draw()
--- planet.lua
+++ planet.lua
@@ -1,35 +1,40 @@
-function Planet(id, pos, r, d)
+function Planet(id, pos, vel, r, d)
   local p = {}
   p.id    = id
   p.r     = r
   p.d     = d
 
   p.body    = love.physics.newBody(world, pos.x, pos.y, "dynamic")
+  p.body:setLinearVelocity(vel.x, vel.y)
   p.shape   = love.physics.newCircleShape(r)
-  p.fixture = love.physics.newFixture(p.body, p.shape)
+  p.fixture = love.physics.newFixture(p.body, p.shape, p.d)
+  p.fixture:setRestitution(0.2)
+  p.fixture:setFriction(1)
 
   function p:draw()
-    love.graphics.setColor({1, 1, 1})
-    print(self.body:getX(), self.body:getY())
-    love.graphics.circle("line", self.body:getX(), self.body:getY(), self.r)
+    lg.setColor({1, 1, 1})
+    lg.circle("line", self.body:getX(), self.body:getY(), self.r)
   end
 
   function p:getGravForce(other)
-    local dist = love.physics.getDistance(self.fixture, other.fixture)
-    local angle = getAngle(self.body:getX(), self.body:getY(), other.body:getX(), other.body:getY())
+    local xDist = other.body:getX() - self.body:getX()
+    local yDist = other.body:getY() - self.body:getY()
+
+    local dist  = love.physics.getDistance(self.fixture, other.fixture) + self.r + other.r
     local F = (G * self.body:getMass() * other.body:getMass())/(dist*dist)
-    return F*math.Sin(angle), F*math.Cos(angle)
+
+    return F*xDist, F*yDist
   end
 
   function p:update()
     local fTotalX, fTotalY = 0, 0
-    for i, #planets do
-      if planets[i].id != self.id then
-        local dx, dy = self.getGravForce(planets[i])
+    for i=1, #planets do
+      if planets[i].id ~= self.id then
+        local dx, dy = self:getGravForce(planets[i])
         fTotalX, fTotalY = fTotalX + dx, fTotalY + dy
       end
     end
-    self.body.applyForce(fTotalX, fTotalY)
+    self.body:applyForce(fTotalX, fTotalY)
   end
 
   table.insert(planets, p)
