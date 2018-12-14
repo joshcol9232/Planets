3759fe79b5c91707f5b18cd3d828eb1a1b7f0493
12ColcloughJ <12colcloughj@lytchett.org.uk>
2018-12-14 17:32:44 +0000

    Made it a bit more usable.
    Can toggle debug modes, and also spawn planets with velocity using the mouse, or right click to spawn 100 in a row cause idk

--- main.lua
+++ main.lua
@@ -8,6 +8,9 @@ lg = love.graphics
 function love.load()
   love.window.setMode(1000, 700)
 
+  VEL_DEBUG = false
+  FORCE_DEBUG = false
+  mouseX, mouseY = 0, 0
   love.physics.setMeter(1)
   world = love.physics.newWorld(0, 0, true)
   planets = {}
@@ -16,6 +19,32 @@ function love.load()
 
   --update_rate = 1/60
   --update_timer = 0
+end
+
+function love.mousepressed(x, y, button)
+  mouseX, mouseY = x, y
+end
+
+function love.mousereleased(x, y, button)
+  if button == 1 then
+    Planet(#planets+1, Vec2(mouseX, mouseY), Vec2(mouseX-x, mouseY-y), 10, 100)
+  end
+
+  if button == 2 then
+    for i=0, 99 do
+      Planet(#planets+1, Vec2(mouseX+(i*5), mouseY), Vec2(mouseX-x, mouseY-y), 5, 100)
+    end
+  end
+end
+
+function love.keypressed(key)
+  if key == "c" then
+    planets = {}
+  elseif key == "1" then
+    FORCE_DEBUG = not FORCE_DEBUG
+  elseif key == "2" then
+    VEL_DEBUG = not VEL_DEBUG
+  end
 end
 
 function love.update(dt)
@@ -32,7 +61,31 @@ end
 function love.draw()
   lg.setColor({1, 1, 1})
   lg.print(love.timer.getFPS(), 10, 10)
+  lg.print("Object Count: "..#planets, 10, 24)
+
+  if FORCE_DEBUG then
+    lg.setColor({1, 0, 0})
+  else
+    lg.setColor({0.3, 0.3, 0.3})
+  end
+  lg.print("1: Force debug", 10, 40)
+
+  if VEL_DEBUG then
+    lg.setColor({0, 1, 0})
+  else
+    lg.setColor({0.3, 0.3, 0.3})
+  end
+  lg.print("2: Velocity debug", 10, 54)
+
+
+
   for i=1, #planets do
     planets[i]:draw()
+    if VEL_DEBUG then
+      planets[i]:debugVel()
+    end
+    if FORCE_DEBUG then
+      planets[i]:debugForce()
+    end
   end
 end
