cd1eb9ce00ed9e05af26ea64c2c1fef7f274afc4
eggmund <>
2019-04-14 17:24:13 +0100

    Working and added prediction

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,53 @@
 use raylib::{Color, Vector2, RaylibHandle, consts};
 
 mod planet;
+use planet::{Planet, PLANET_DENSITY};
 
-const G: f32 = 0.1;
+const G: f32 = 0.001;
+
+struct Prediction {
+	body: Planet,
+	nodes: Vec<Vector2>,
+	colliding: bool
+}
+
+impl Default for Prediction {
+	fn default() -> Prediction {
+		Prediction {
+			body: Planet::default(),
+			nodes: vec![],
+			colliding: false
+		}
+	}
+}
+
+impl Prediction {
+	pub fn reset(&mut self, pos: Vector2, vel: Vector2) {
+		self.body.pos = pos;
+		self.body.vel = vel;
+		self.colliding = false;
+		self.nodes = vec![];
+	}
+
+	pub fn draw(&self, rl: &RaylibHandle) {
+		for i in 0..self.nodes.len() {
+			if i > 0 {
+				rl.draw_line_ex(self.nodes[i-1], self.nodes[i], 1.0, Color::RED);
+			}
+		}
+	}
+}
 
 struct App {
 	rl: RaylibHandle,
-	planets: Vec<planet::Planet>,
+	planets: Vec<Planet>,
+	collided_bodies: Vec<u32>, // ID's
+	pl_id_count: u32,
+	mouse_click_pos: Vector2,
+	last_mouse_pos: Vector2,
+	paused: bool,
+
+	prediction: Prediction,
 }
 
 impl App {
@@ -14,6 +55,13 @@ impl App {
 		App {
 			rl: ray,
 			planets: vec![],
+			collided_bodies: vec![],
+			pl_id_count: 0,
+			mouse_click_pos: Vector2::zero(),
+			last_mouse_pos: Vector2::zero(),
+			paused: false,
+
+			prediction: Prediction::default()
 		}
 	}
 
@@ -31,21 +79,58 @@ impl App {
 			self.rl.end_drawing();
 		}
 	}
+
+	pub fn add_planet(&mut self, pos: Vector2, vel: Vector2, rad: f32) {
+		self.planets.push( planet::Planet::new(self.pl_id_count, pos, vel, rad, 0.0) );
+		self.pl_id_count += 1;
+	}
 	
 	pub fn update(&mut self, dt: f32) {
-		for i in 0..self.planets.len() {   // For each planet
-			for j in i..self.planets.len() {  // For every other planet
-				if i != j {
-					println!("i, j: {} {}", i, j);
-					let (df1, df2) = self.get_grav_force_between_two_planets(i, j);
-					self.planets[i].res_force += df1;
-					self.planets[j].res_force += df2;
+		if self.pl_id_count >= 4294967294 {
+			self.pl_id_count = 0;
+		}
 
-					println!("Egg: {} {}", df1.x, df2.x);
-				}
+		self.kill_dead_planets();
+
+
+		if self.rl.is_mouse_button_down(0) {
+			if !self.prediction.colliding {
+				self.update_prediction(dt * 12.0);
 			}
 
-			self.planets[i].update(dt);
+			if (self.rl.get_mouse_position() - self.last_mouse_pos).length() > 3.0 {
+				self.last_mouse_pos = self.rl.get_mouse_position();
+				self.prediction.reset(self.mouse_click_pos, self.mouse_click_pos - self.last_mouse_pos);
+			}
+		}
+
+		if self.rl.is_mouse_button_pressed(0) {
+			self.mouse_click_pos = self.rl.get_mouse_position();
+			self.last_mouse_pos = self.mouse_click_pos;
+			self.prediction.reset(self.mouse_click_pos, Vector2::zero());
+			self.paused = true;
+		} else if self.rl.is_mouse_button_released(0) {
+			self.add_planet(self.mouse_click_pos, self.mouse_click_pos - self.rl.get_mouse_position(), 5.0);
+			self.paused = false;
+			self.prediction.reset(Vector2::zero(), Vector2::zero());
+		}
+
+		if !self.paused {
+			for i in 0..self.planets.len() {   // For each planet
+				for j in i..self.planets.len() {  // For every other planet
+					if i != j && !self.collided_bodies.contains(&self.planets[j].id) {
+						if self.check_for_collision(&self.planets[i], &self.planets[j]) && !self.collided_bodies.contains(&self.planets[i].id)  {
+							self.collide(i, j);
+						} else {
+							let (df1, df2) = self.get_grav_force_between_two_planets(&self.planets[i], &self.planets[j]);
+							self.planets[i].res_force += df1;
+							self.planets[j].res_force += df2;
+						}
+					}
+				}
+
+				self.planets[i].update(dt);
+			}
 		}
 	}
 
@@ -53,22 +138,100 @@ impl App {
 		for p in self.planets.iter() {
 			p.draw(&self.rl);
 		}
+
+		if self.rl.is_mouse_button_down(0) {
+			self.rl.draw_line_ex(self.mouse_click_pos, self.rl.get_mouse_position(), 2.0, Color::GREEN);
+			self.prediction.draw(&self.rl);
+		}
+
+		self.rl.draw_text(format!("Body num: {}", self.planets.len()).as_str(), 10, 36, 20, Color::RAYWHITE);
+		self.rl.draw_text(format!("Prediction nodes: {}", self.prediction.nodes.len()).as_str(), 10, 54, 20, Color::RAYWHITE);
 	}
 
-	fn get_grav_force_between_two_planets(&self, i1: usize, i2: usize) -> (Vector2, Vector2) {   // returns force on pl1 and pl2
-		let pos = self.planets[i2].pos - self.planets[i1].pos;
-		let angle = self.planets[i1].pos.angle_to(self.planets[i2].pos);
+	fn update_prediction(&mut self, dt: f32) {
+		for i in 0..self.planets.len() {
+			let (force, _) = self.get_grav_force_between_two_planets(&self.prediction.body, &self.planets[i]);
+			self.prediction.body.res_force += force;
+
+			self.prediction.colliding = self.check_for_collision(&self.prediction.body, &self.planets[i]);
+			if self.prediction.colliding {
+				break;
+			}
+		}
+
+		self.prediction.body.update(dt);
+
+		if self.prediction.nodes.len() < 2000 {
+			self.prediction.nodes.push(self.prediction.body.pos);
+		}
+	}
+
+
+	fn kill_dead_planets(&mut self) {
+		let col_bods = self.collided_bodies.clone();
+		self.planets.retain(|pl| !col_bods.contains(&pl.id));
+		self.collided_bodies = vec![];
+	}
+
+	pub fn check_for_collision(&self, p1: &Planet, p2: &Planet) -> bool {
+		(p2.pos - p1.pos).length() <= p1.radius + p2.radius
+	}
+
+	fn collide(&mut self, p1: usize, p2: usize) {
+		let total_momentum = self.planets[p1].get_momentum() + self.planets[p2].get_momentum();
+
+		let p1_volume = self.planets[p1].mass/PLANET_DENSITY;
+		let p2_volume = self.planets[p2].mass/PLANET_DENSITY;
+
+		let new_rad = (((3.0/4.0) * (p1_volume + p2_volume))/consts::PI as f32).powf(1.0/3.0);
+		let total_mass = self.planets[p1].mass + self.planets[p2].mass;
+
+		self.planets.push(Planet::new(
+			self.pl_id_count,
+			if self.planets[p1].radius > self.planets[p2].radius + 3.0 {
+				self.planets[p1].pos
+			} else if (self.planets[p1].radius - self.planets[p2].radius).abs() < 3.0 {
+				(self.planets[p1].pos + self.planets[p2].pos)/2.0
+			} else {
+				self.planets[p2].pos
+			},
+			total_momentum/total_mass,
+			new_rad,
+			total_mass,
+		));
+
+		self.pl_id_count += 1;
+
+		self.collided_bodies.push(self.planets[p1].id);
+		self.collided_bodies.push(self.planets[p2].id);
+	}
+
+	pub fn get_grav_force_between_two_planets(&self, p1: &Planet, p2: &Planet) -> (Vector2, Vector2) {   // returns force on pl1 and pl2
+		let pos = p2.pos - p1.pos;
+		let angle = p1.pos.angle_to(p2.pos);
 		let angle2 = angle + consts::PI as f32;
-		let f_mag = (G * self.planets[i1].mass * self.planets[i2].mass)/(pos.length().powi(2));
-		
-		(Vector2 { x: f_mag * angle.cos(), y: f_mag * angle.sin() }, Vector2 { x: f_mag * angle2.cos(), y: f_mag * angle2.sin() })
+		let dist = pos.length();
+		if dist > p1.radius + p2.radius {
+			let f_mag = (G * p1.mass * p2.mass)/(dist.powi(2));
+			
+			(Vector2 { x: f_mag * angle.cos(), y: f_mag * angle.sin() }, Vector2 { x: f_mag * angle2.cos(), y: f_mag * angle2.sin() })
+		} else {
+			(Vector2::zero(), Vector2::zero())
+		}
 	}
 
-	pub fn add_planet(&mut self, pos: Vector2, vel: Vector2, rad: f32) {
-		self.planets.push( planet::Planet::new(pos, vel, rad) );
+	pub fn make_square(&mut self, pos: Vector2, rad: f32, sep: f32, w: u32, h: u32) {
+		for i in 0..w {
+			for j in 0..h {
+				self.add_planet(
+					Vector2 { x: i as f32 * (rad + sep) * 2.0, y: j as f32 * (rad + sep) * 2.0 } + pos,
+					Vector2::zero(),
+					rad
+				);
+			}
+		}
 	}
 }
-
 
 fn main() {
 	let rl = raylib::init()
@@ -79,11 +242,15 @@ fn main() {
 	rl.set_target_fps(144);
 
 	let mut a = App::new(rl);
-	a.add_planet(Vector2 { x: 300.0, y: 310.0 }, Vector2 { x: 10.0, y: 10.0 }, 5.0);
-	a.add_planet(Vector2 { x: 500.0, y: 300.0 }, Vector2::zero(), 5.0);
-	a.add_planet(Vector2 { x: 700.0, y: 300.0 }, Vector2::zero(), 5.0);
-	a.add_planet(Vector2 { x: 400.0, y: 700.0 }, Vector2::zero(), 5.0);
-	a.add_planet(Vector2 { x: 400.0, y: 500.0 }, Vector2::zero(), 5.0);
+	
+	a.add_planet(Vector2 { x: 300.0, y: 310.0 }, Vector2::zero(), 50.0);
+	//a.add_planet(Vector2 { x: 500.0, y: 300.0 }, Vector2::zero(), 5.0);
+	//a.add_planet(Vector2 { x: 700.0, y: 300.0 }, Vector2::zero(), 5.0);
+	//a.add_planet(Vector2 { x: 400.0, y: 700.0 }, Vector2::zero(), 5.0);
+	//a.add_planet(Vector2 { x: 400.0, y: 500.0 }, Vector2::zero(), 5.0);
+	
+
+	//a.make_square(Vector2{ x: 200.0, y: 300.0 }, 2.0, 10.0, 20, 20);
 
 	a.main_loop();
 }
--- src/planet.rs
+++ src/planet.rs
@@ -1,29 +1,55 @@
 use raylib::{Vector2, RaylibHandle, Color, consts};
 
-const PLANET_DENSITY: f32 = 5000.0; // kg m^-2
+pub const PLANET_DENSITY: f32 = 5000.0; // kg m^-2
 
 pub struct Planet {
+	pub id: u32,
 	pub pos: Vector2,
-	vel: Vector2,
-	radius: f32,
+	pub vel: Vector2,
+	pub radius: f32,
 	pub mass: f32,
-	pub res_force: Vector2  // Resultant force
+	pub res_force: Vector2,  // Resultant force
+
+	pub has_merged: bool,  // For removing
 }
 
-impl Planet {
-	pub fn new(p: Vector2, v: Vector2, r: f32) -> Planet {
+impl Default for Planet {
+	fn default() -> Planet {
 		Planet {
+			id: 0,
+			pos: Vector2::zero(),
+			vel: Vector2::zero(),
+			radius: 2.0,
+			mass: Planet::get_mass(2.0),
+			res_force: Vector2::zero(),
+			has_merged: false
+		}
+	}
+}
+
+impl PartialEq for Planet {
+	fn eq(&self, other: &Planet) -> bool {
+		self.id == other.id
+	}
+}
+
+impl Eq for Planet {}
+
+impl Planet {
+	pub fn new(id_num: u32, p: Vector2, v: Vector2, r: f32, ma: f32) -> Planet {
+		Planet {
+			id: id_num,
 			pos: p,
 			vel: v,
 			radius: r,
-			mass: Planet::get_mass(r),
-			res_force: Vector2::zero()
+			mass: if ma == 0.0 { Planet::get_mass(r) } else { ma },
+			..Default::default()
 		}
 	}
 
 	pub fn draw(&self, rl: &RaylibHandle) {
 		rl.draw_circle_v(self.pos, self.radius, Color::RAYWHITE);
-		self.draw_debug(rl);
+		//self.draw_debug(rl);
 	}
 
 	pub fn update(&mut self, dt: f32) {
@@ -33,13 +59,15 @@ impl Planet {
 		self.pos += self.vel.scale_by(dt);
 	}
 
-	fn get_mass(radius: f32) -> f32 {  // Area is mass cause 2d
-		consts::PI as f32 * (radius.powi(2)) * PLANET_DENSITY
+	fn get_mass(radius: f32) -> f32 {
+		4.0/3.0 * consts::PI as f32 * radius.powi(3) * PLANET_DENSITY
 	}
 
 	fn draw_debug(&self, rl: &RaylibHandle) {
-		rl.draw_line_ex(self.pos, (self.res_force) + self.pos, 2.0, Color::RED);
 		rl.draw_line_ex(self.pos, self.vel + self.pos, 2.0, Color::LIME);
 	}
 
+	pub fn get_momentum(&self) -> Vector2 {
+		self.vel.scale_by(self.mass)
+	}
 }
