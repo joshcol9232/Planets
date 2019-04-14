82d7b6f55b09bdf7e8a6104fe8a8d30116b01451
eggmund <>
2019-04-14 20:36:42 +0100

    Added trails

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,35 @@
 use raylib::{Color, Vector2, RaylibHandle, consts};
 
 mod planet;
-use planet::{Planet, PLANET_DENSITY};
+use planet::{Planet, PLANET_DENSITY, TrailNode};
 
 const G: f32 = 0.001;
 
 struct Prediction {
 	body: Planet,
-	nodes: Vec<Vector2>,
 	colliding: bool
 }
 
 impl Default for Prediction {
 	fn default() -> Prediction {
 		Prediction {
-			body: Planet::default(),
-			nodes: vec![],
+			body: Planet { is_prediction: true, ..Default::default() },
 			colliding: false
 		}
 	}
 }
 
 impl Prediction {
-	pub fn reset(&mut self, pos: Vector2, vel: Vector2) {
+	pub fn reset(&mut self, pos: Vector2, vel: Vector2, rad: f32) {
 		self.body.pos = pos;
 		self.body.vel = vel;
+		self.body.radius = rad;
 		self.colliding = false;
-		self.nodes = vec![];
+		self.body.trail_nodes = vec![TrailNode { pos: pos, time_created: 0.0 }];
 	}
 
-	pub fn draw(&self, rl: &RaylibHandle) {
-		for i in 0..self.nodes.len() {
-			if i > 0 {
-				rl.draw_line_ex(self.nodes[i-1], self.nodes[i], 1.0, Color::RED);
-			}
-		}
+	pub fn draw(&self, rl: &RaylibHandle, time: f32) {
+		self.body.draw(rl, time);
 	}
 }
 
@@ -46,6 +41,8 @@ struct App {
 	mouse_click_pos: Vector2,
 	last_mouse_pos: Vector2,
 	paused: bool,
+	planet_spawn_size: f32,
+	unpaused_time: f32,
 
 	prediction: Prediction,
 }
@@ -59,7 +56,9 @@ impl App {
 			pl_id_count: 0,
 			mouse_click_pos: Vector2::zero(),
 			last_mouse_pos: Vector2::zero(),
-			paused: false,
+			paused: true,
+			planet_spawn_size: 5.0,
+			unpaused_time: 0.0,
 
 			prediction: Prediction::default()
 		}
@@ -80,8 +79,8 @@ impl App {
 		}
 	}
 
-	pub fn add_planet(&mut self, pos: Vector2, vel: Vector2, rad: f32) {
-		self.planets.push( planet::Planet::new(self.pl_id_count, pos, vel, rad, 0.0) );
+	pub fn add_planet(&mut self, pos: Vector2, vel: Vector2, rad: f32, stationary: bool) {
+		self.planets.push( planet::Planet::new(self.pl_id_count, pos, vel, rad, 0.0, stationary) );
 		self.pl_id_count += 1;
 	}
 	
@@ -92,30 +91,10 @@ impl App {
 
 		self.kill_dead_planets();
 
-
-		if self.rl.is_mouse_button_down(0) {
-			if !self.prediction.colliding {
-				self.update_prediction(dt * 12.0);
-			}
-
-			if (self.rl.get_mouse_position() - self.last_mouse_pos).length() > 3.0 {
-				self.last_mouse_pos = self.rl.get_mouse_position();
-				self.prediction.reset(self.mouse_click_pos, self.mouse_click_pos - self.last_mouse_pos);
-			}
-		}
-
-		if self.rl.is_mouse_button_pressed(0) {
-			self.mouse_click_pos = self.rl.get_mouse_position();
-			self.last_mouse_pos = self.mouse_click_pos;
-			self.prediction.reset(self.mouse_click_pos, Vector2::zero());
-			self.paused = true;
-		} else if self.rl.is_mouse_button_released(0) {
-			self.add_planet(self.mouse_click_pos, self.mouse_click_pos - self.rl.get_mouse_position(), 5.0);
-			self.paused = false;
-			self.prediction.reset(Vector2::zero(), Vector2::zero());
-		}
+		self.get_input(dt);
 
 		if !self.paused {
+			self.unpaused_time += dt;
 			for i in 0..self.planets.len() {   // For each planet
 				for j in i..self.planets.len() {  // For every other planet
 					if i != j && !self.collided_bodies.contains(&self.planets[j].id) {
@@ -129,23 +108,64 @@ impl App {
 					}
 				}
 
-				self.planets[i].update(dt);
+				self.planets[i].update(dt, self.unpaused_time);
 			}
 		}
 	}
 
 	pub fn draw(&self) {
 		for p in self.planets.iter() {
-			p.draw(&self.rl);
+			p.draw(&self.rl, self.unpaused_time);
 		}
 
 		if self.rl.is_mouse_button_down(0) {
 			self.rl.draw_line_ex(self.mouse_click_pos, self.rl.get_mouse_position(), 2.0, Color::GREEN);
-			self.prediction.draw(&self.rl);
+			self.prediction.draw(&self.rl, self.unpaused_time);
 		}
 
-		self.rl.draw_text(format!("Body num: {}", self.planets.len()).as_str(), 10, 36, 20, Color::RAYWHITE);
-		self.rl.draw_text(format!("Prediction nodes: {}", self.prediction.nodes.len()).as_str(), 10, 54, 20, Color::RAYWHITE);
+		self.rl.draw_text(format!("Bodies: {}", self.planets.len()).as_str(), 10, 36, 20, Color::RAYWHITE);
+		self.rl.draw_text(format!("Spawn size: {}", self.planet_spawn_size).as_str(), 10, 58, 20, Color::RAYWHITE);
+		self.rl.draw_text(format!("Trail nodes: {}", self.get_trail_node_total()).as_str(), 10, 80, 20, Color::RAYWHITE);
+	}
+
+	fn get_input(&mut self, dt: f32) {
+		if self.rl.is_mouse_button_down(0) {
+			if !self.prediction.colliding {
+				self.update_prediction(dt * 12.0);
+			}
+
+			if (self.rl.get_mouse_position() - self.last_mouse_pos).length() >= 2.0 {
+				self.last_mouse_pos = self.rl.get_mouse_position();
+				self.prediction.reset(self.mouse_click_pos, self.mouse_click_pos - self.last_mouse_pos, self.planet_spawn_size);
+			}
+		}
+
+		if self.rl.is_mouse_button_pressed(0) {
+			self.mouse_click_pos = self.rl.get_mouse_position();
+			self.last_mouse_pos = self.mouse_click_pos;
+			self.prediction.reset(self.mouse_click_pos, Vector2::zero(), self.planet_spawn_size);
+			self.paused = true;
+		} else if self.rl.is_mouse_button_released(0) {
+			self.add_planet(self.mouse_click_pos, self.mouse_click_pos - self.rl.get_mouse_position(), self.planet_spawn_size, false);
+			self.paused = false;
+			self.prediction.reset(Vector2::zero(), Vector2::zero(), self.planet_spawn_size);
+		}
+
+		if self.rl.is_mouse_button_pressed(1) {
+			self.add_planet(self.rl.get_mouse_position(), Vector2::zero(), self.planet_spawn_size, true);
+		}
+
+		if self.rl.is_key_pressed(consts::KEY_UP as i32) {
+			self.planet_spawn_size += 1.0;
+		} else if self.rl.is_key_pressed(consts::KEY_DOWN as i32) {
+			if self.planet_spawn_size >= 1.0 {
+				self.planet_spawn_size -= 1.0;
+			}
+		}
+
+		if self.rl.is_key_pressed(consts::KEY_P as i32) {
+			self.paused = !self.paused;
+		}
 	}
 
 	fn update_prediction(&mut self, dt: f32) {
@@ -159,13 +179,16 @@ impl App {
 			}
 		}
 
-		self.prediction.body.update(dt);
-
-		if self.prediction.nodes.len() < 2000 {
-			self.prediction.nodes.push(self.prediction.body.pos);
-		}
+		self.prediction.body.update(dt, 0.0);
 	}
 
+	fn get_trail_node_total(&self) -> usize {
+		let mut total = 0;
+		for p in self.planets.iter() {
+			total += p.trail_nodes.len();
+		}
+		total
+	}
 
 	fn kill_dead_planets(&mut self) {
 		let col_bods = self.collided_bodies.clone();
@@ -186,18 +209,25 @@ impl App {
 		let new_rad = (((3.0/4.0) * (p1_volume + p2_volume))/consts::PI as f32).powf(1.0/3.0);
 		let total_mass = self.planets[p1].mass + self.planets[p2].mass;
 
+		let considerable_diference = (self.planets[p1].radius.max(self.planets[p2].radius)/self.planets[p1].radius.min(self.planets[p2].radius)).powi(3) > 2.0; // If largest more than x times more volume (proportionaly)
+
 		self.planets.push(Planet::new(
 			self.pl_id_count,
-			if self.planets[p1].radius > self.planets[p2].radius + 3.0 {
-				self.planets[p1].pos
-			} else if (self.planets[p1].radius - self.planets[p2].radius).abs() < 3.0 {
+			if !considerable_diference {
 				(self.planets[p1].pos + self.planets[p2].pos)/2.0
+			} else if self.planets[p1].radius > self.planets[p2].radius {
+				self.planets[p1].pos
 			} else {
 				self.planets[p2].pos
 			},
 			total_momentum/total_mass,
 			new_rad,
 			total_mass,
+			if self.planets[p1].radius > self.planets[p2].radius {
+				self.planets[p1].stationary
+			} else {
+				self.planets[p2].stationary
+			}
 		));
 
 		self.pl_id_count += 1;
@@ -220,13 +250,14 @@ impl App {
 		}
 	}
 
-	pub fn make_square(&mut self, pos: Vector2, rad: f32, sep: f32, w: u32, h: u32) {
+	pub fn make_square(&mut self, pos: Vector2, stat: bool, rad: f32, sep: f32, w: u32, h: u32) {
 		for i in 0..w {
 			for j in 0..h {
 				self.add_planet(
 					Vector2 { x: i as f32 * (rad + sep) * 2.0, y: j as f32 * (rad + sep) * 2.0 } + pos,
 					Vector2::zero(),
-					rad
+					rad,
+					stat
 				);
 			}
 		}
@@ -235,22 +266,19 @@ impl App {
 
 fn main() {
 	let rl = raylib::init()
-		.size(1000, 800)
+		.size(1920, 1080)
 		.title("N-body")
+		.msaa_4x()
 		.build();
 
-	rl.set_target_fps(144);
+	rl.set_target_fps(144 * 2);
 
 	let mut a = App::new(rl);
 	
-	a.add_planet(Vector2 { x: 300.0, y: 310.0 }, Vector2::zero(), 50.0);
-	//a.add_planet(Vector2 { x: 500.0, y: 300.0 }, Vector2::zero(), 5.0);
-	//a.add_planet(Vector2 { x: 700.0, y: 300.0 }, Vector2::zero(), 5.0);
-	//a.add_planet(Vector2 { x: 400.0, y: 700.0 }, Vector2::zero(), 5.0);
-	//a.add_planet(Vector2 { x: 400.0, y: 500.0 }, Vector2::zero(), 5.0);
-	
+	a.add_planet(Vector2 { x: 740.0, y: 540.0 }, Vector2::zero(), 40.0, true);
+	a.add_planet(Vector2 { x: 1180.0, y: 540.0 }, Vector2::zero(), 40.0, true);
 
-	//a.make_square(Vector2{ x: 200.0, y: 300.0 }, 2.0, 10.0, 20, 20);
+	a.make_square(Vector2{ x: 840.0, y: 900.0 }, false, 1.0, 10.0, 12, 10);
 
 	a.main_loop();
 }
--- src/planet.rs
+++ src/planet.rs
@@ -1,6 +1,13 @@
 use raylib::{Vector2, RaylibHandle, Color, consts};
 
 pub const PLANET_DENSITY: f32 = 5000.0; // kg m^-2
+const TRAIL_PLACEMENT_INTERVAL: f32 = 0.04;
+const TRAIL_LIFESPAN: f32 = 1.0;
+
+pub struct TrailNode {
+	pub pos: Vector2,
+	pub time_created: f32
+}
 
 pub struct Planet {
 	pub id: u32,
@@ -9,8 +16,11 @@ pub struct Planet {
 	pub radius: f32,
 	pub mass: f32,
 	pub res_force: Vector2,  // Resultant force
+	pub stationary: bool,
+	pub is_prediction: bool,
 
-	pub has_merged: bool,  // For removing
+	pub trail_nodes: Vec<TrailNode>,
+	pub trail_timer: f32
 }
 
 impl Default for Planet {
@@ -19,10 +29,13 @@ impl Default for Planet {
 			id: 0,
 			pos: Vector2::zero(),
 			vel: Vector2::zero(),
-			radius: 2.0,
+			radius: 5.0,
 			mass: Planet::get_mass(2.0),
 			res_force: Vector2::zero(),
-			has_merged: false
+			stationary: false,
+			is_prediction: false,
+			trail_nodes: vec![],
+			trail_timer: 0.0,
 		}
 	}
 }
@@ -36,37 +49,84 @@ impl PartialEq for Planet {
 impl Eq for Planet {}
 
 impl Planet {
-	pub fn new(id_num: u32, p: Vector2, v: Vector2, r: f32, ma: f32) -> Planet {
+	pub fn new(id_num: u32, p: Vector2, v: Vector2, r: f32, ma: f32, stat: bool) -> Planet {
 		Planet {
 			id: id_num,
 			pos: p,
 			vel: v,
 			radius: r,
 			mass: if ma == 0.0 { Planet::get_mass(r) } else { ma },
+			stationary: stat,
+			is_prediction: false,
 			..Default::default()
 		}
 	}
 
-	pub fn draw(&self, rl: &RaylibHandle) {
-		rl.draw_circle_v(self.pos, self.radius, Color::RAYWHITE);
+	pub fn draw(&self, rl: &RaylibHandle, time: f32) {
+		self.draw_trail(rl, time);
+		let col = if self.is_prediction { Color::RED } else { Color::RAYWHITE };
+		
+		rl.draw_circle_v(self.pos, self.radius, col);
 		//self.draw_debug(rl);
 	}
 
-	pub fn update(&mut self, dt: f32) {
-		self.vel += self.res_force.scale_by(dt/self.mass);  // F = ma, a = F / m, a * dt = vel increase, f * dt/mass = vel change
-		self.res_force = Vector2::zero();
-	
-		self.pos += self.vel.scale_by(dt);
+	pub fn update(&mut self, dt: f32, time: f32) {
+		if !self.is_prediction {
+			self.kill_dead_trails(time);
+		}
+
+		if !self.stationary {
+			self.trail_timer += dt;
+			if self.trail_timer >= TRAIL_PLACEMENT_INTERVAL {
+				self.place_trail(time);
+				self.trail_timer = 0.0;
+			}
+
+			let len = self.trail_nodes.len();
+			if len > 0 {
+				self.trail_nodes[len-1].pos = self.pos;
+			}
+
+			self.vel += self.res_force.scale_by(dt/self.mass);  // F = ma, a = F / m, a * dt = vel increase, f * dt/mass = vel change
+			self.res_force = Vector2::zero();
+		
+			self.pos += self.vel.scale_by(dt);
+		}
 	}
 
+	fn draw_trail(&self, rl: &RaylibHandle, time: f32) {
+		let mut col = if self.is_prediction { Color::RED } else { Color::BLUE };
+		for i in 0..self.trail_nodes.len() {
+			if i > 0 {
+				if !self.is_prediction {
+					col.a = ((1.0 - ((time - self.trail_nodes[i].time_created)/TRAIL_LIFESPAN)).powi(2) * 255.0).min(255.0) as u8;
+				}
+				rl.draw_line_ex(self.trail_nodes[i-1].pos, self.trail_nodes[i].pos, 1.0, col);
+			}
+		}
+	}
+
+	#[inline]
+	fn place_trail(&mut self, time: f32) {
+		self.trail_nodes.push(TrailNode { pos: self.pos, time_created: time });
+	}
+
+	#[inline]
+	fn kill_dead_trails(&mut self, time: f32) {
+		self.trail_nodes.retain(|t|time - t.time_created <= TRAIL_LIFESPAN);
+	}
+
+	#[inline]
 	fn get_mass(radius: f32) -> f32 {
 		4.0/3.0 * consts::PI as f32 * radius.powi(3) * PLANET_DENSITY
 	}
 
+	#[inline]
 	fn draw_debug(&self, rl: &RaylibHandle) {
 		rl.draw_line_ex(self.pos, self.vel + self.pos, 2.0, Color::LIME);
 	}
 
+	#[inline]
 	pub fn get_momentum(&self) -> Vector2 {
 		self.vel.scale_by(self.mass)
 	}
